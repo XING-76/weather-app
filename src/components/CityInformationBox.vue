dcdbb5b2dfea6868ef1f249ad22e6790519c9a11
XING <jackjohnton789@gmail.com>
2023-09-19 17:53:30 +0800

    fix CityInformationBox redundant code

--- src/components/CityInformationBox.vue
+++ src/components/CityInformationBox.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="flex flex-col items-center text-white p-6">
-        <div class="text-2xl mb-2">{{ cityInformation.cityName }}</div>
+        <div class="text-2xl mb-2">{{ cityInformationItem.cityName }}</div>
 
         <div v-if="!isVisible">
             <div class="text-sm mb-2">
-                {{ cityInformation.temperature }} |
-                {{ cityInformation.description }}
+                {{ cityInformationItem.temperature }} |
+                {{ cityInformationItem.description }}
             </div>
         </div>
 
         <div v-if="isVisible">
-            <div class="text-5xl mb-2">{{ cityInformation.temperature }}</div>
+            <div class="text-5xl mb-2">
+                {{ cityInformationItem.temperature }}
+            </div>
 
             <div class="text-xs">
-                <div class="mb-1">{{ cityInformation.description }}</div>
+                <div class="mb-1">{{ cityInformationItem.description }}</div>
                 <div class="flex justify-center gap-2">
-                    <span>H:{{ cityInformation.maxTemperature }}</span>
-                    <span>L:{{ cityInformation.minTemperature }}</span>
+                    <span>H:{{ cityInformationItem.maxTemperature }}</span>
+                    <span>L:{{ cityInformationItem.minTemperature }}</span>
                 </div>
             </div>
         </div>
@@ -24,15 +26,13 @@
 </template>
 
 <script setup lang="ts">
-import { toRefs, reactive } from 'vue';
+import { toRefs } from 'vue';
 import { useMainStore } from '@/store';
 import { storeToRefs } from 'pinia';
 
 const store = useMainStore();
 
 const { cityInformationItem } = storeToRefs(store);
-
-const cityInformation = reactive(cityInformationItem);
 
 const props = defineProps({
     isVisible: { type: Boolean, required: true },
