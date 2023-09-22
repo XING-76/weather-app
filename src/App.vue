c510a555a24cbe58837dba78f3759a1c8b91c413
XING <jackjohnton789@gmail.com>
2023-09-22 15:16:16 +0800

    update Project margin style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.bg-gradient-to-b.mx-auto.text-center.font-bold(class='from-[#415d99] to-[#a3a9c1] md:my-8 md:w-[85%]')
+.bg-gradient-to-b.mx-auto.text-center.font-bold(class='from-[#415d99] to-[#a3a9c1] md:w-[85%]')
     RouterView
 #container
 </template>
